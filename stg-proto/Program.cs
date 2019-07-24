b0edce4e79bd016b56d2d4b32a22197179f55819
mamepuro <beans.mamegame.laugh@icloud.com>
2019-07-24 18:52:15 +0900

    test

---
+++ stg-proto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stgproto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
